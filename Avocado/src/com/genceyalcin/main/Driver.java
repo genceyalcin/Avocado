1c57bcaddf9c7aefff24edff0260ad202edff885
Hamza Butt <zahamza05@gmail.com>
2020-05-10 01:49:11 +0400

    Created a cube with a mesh

--- Avocado/src/com/genceyalcin/main/Driver.java
+++ Avocado/src/com/genceyalcin/main/Driver.java
@@ -1,5 +1,14 @@
 package com.genceyalcin.main;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.genceyalcin.util3D.Mesh;
+import com.genceyalcin.util3D.Polygon;
+import com.genceyalcin.util3D.Triangle;
+import com.genceyalcin.util3D.Vector3F;
+
 /**
  * Runs the driver code for the engine
  * 
@@ -14,8 +23,38 @@ public class Driver {
      * @param args
      */
     public static void main(String args[]) {
+    	
+    	
+        // Make meshes
+        List<Polygon> myList  = new ArrayList<Polygon>();
+        // 
+        myList.add(new Triangle(new Vector3F(1,0,0), new Vector3F(1,1,0), new Vector3F(1,1,1)));
+        myList.add(new Triangle(new Vector3F(1,0,0), new Vector3F(1,0,1), new Vector3F(1,1,1)));
+        //
+        myList.add(new Triangle(new Vector3F(0,0,0), new Vector3F(0,0,1), new Vector3F(1,0,1)));
+        myList.add(new Triangle(new Vector3F(0,0,0), new Vector3F(1,0,0), new Vector3F(1,0,1)));
+        
+        // bottom face
+        myList.add(new Triangle(new Vector3F(0,0,0), new Vector3F(1,0,0), new Vector3F(1,1,0)));
+        myList.add(new Triangle(new Vector3F(0,0,0), new Vector3F(0,1,0), new Vector3F(1,1,0)));
+        //
+        myList.add(new Triangle(new Vector3F(1,1,0), new Vector3F(1,1,1), new Vector3F(0,1,1)));
+        myList.add(new Triangle(new Vector3F(1,1,0), new Vector3F(0,1,0), new Vector3F(0,1,1)));
         
         
+        //
+        myList.add(new Triangle(new Vector3F(0,0,0), new Vector3F(0,0,1), new Vector3F(0,1,1)));
+        myList.add(new Triangle(new Vector3F(0,0,0), new Vector3F(0,1,0), new Vector3F(0,1,1)));
+        
+        // top face
+        myList.add(new Triangle(new Vector3F(0,0,1), new Vector3F(0,1,1), new Vector3F(1,1,1)));
+        myList.add(new Triangle(new Vector3F(0,0,1), new Vector3F(1,0,1), new Vector3F(1,1,1)));
+        
+        
+        Mesh cubeMesh = new Mesh(myList);
+
+
+
         
     }
 
